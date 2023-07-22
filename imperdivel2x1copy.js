26fed09402ed1861b1bf868a205cb172a0ecd49f
BryanChemp <bryan_chemp@hotmail.com>
2023-07-22 17:52:07 -0300

    adicionado novo js

---
+++ imperdivel2x1copy.js
@@ -0,0 +1,21 @@
+let nameInput = document.getElementById("nome-input");
+let deInput = document.getElementById("de-input");
+let porInput = document.getElementById("por-input");
+
+let papeleta_1 = false;
+let papeleta_2 = false;
+let papeleta_4 = false;
+let papeleta_6 = false;
+
+let nameValue;
+let deValue;
+let porValue;
+
+function conversosDe() {
+    console.log("da");
+}
+
+
+if (papeleta_1 == true) {
+    console.log("imprimir");
+}
